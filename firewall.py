cd40d28d42657ca0d72dba856efd7ec79d380dfd
Roy <roywmraziv@icloud.com>
2024-07-23 21:58:22 -0500

    First Commit
    Initial Commit. Code currently not totally functional.

---
+++ firewall.py
@@ -0,0 +1,21 @@
+from scapy.all import sniff, IP
+
+# list of IPs to block/sniff
+blocked_ips = ['192.168.1.75']
+
+def packet_callback(packet):
+    # check if hte packet has an IP layer
+    if IP in packet:
+        src_ip = packet[IP].src
+        dst_ip = packet[IP].dist
+
+    # check if the source or destination IP is in the blocked list
+    if src_ip in blocked_ips or dst_ip in blocked_ips:
+        print(f"Blocked packet: {packet.summary()}")
+    else:
+        print(f"Allowed packet: {packet.summary()}")
+
+print("Starting the firewall...")
+
+# start sniffing packets
+sniff(prn=packet_callback, store = 0)
